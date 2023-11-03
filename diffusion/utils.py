b3fbcce1514a781bdbc0ff6f437e4fabeceb9702
Kai Yi <ykiiiiii@163.com>
2023-11-03 16:01:03 +0800

    add utils for diffusion

---
+++ diffusion/utils.py
@@ -0,0 +1,133 @@
+import torch
+from torch.nn import functional as F
+from torch import sin, cos, atan2, acos
+
+import numpy as np
+import math
+        
+def sum_except_batch(x):
+    return x.reshape(x.size(0), -1).sum(dim=-1)
+
+
+def assert_correctly_masked(variable, node_mask):
+    assert (variable * (1 - node_mask.long())).abs().max().item() < 1e-4, \
+        'Variables not masked properly.'
+
+
+def clip_noise_schedule(alphas2, clip_value=0.001):
+    """
+    For a noise schedule given by alpha^2, this clips alpha_t / alpha_t-1. This may help improve stability during
+    sampling.
+    """
+    alphas2 = np.concatenate([np.ones(1), alphas2], axis=0)
+
+    alphas_step = (alphas2[1:] / alphas2[:-1])
+
+    alphas_step = np.clip(alphas_step, a_min=clip_value, a_max=1.)
+    alphas2 = np.cumprod(alphas_step, axis=0)
+
+    return alphas2
+
+
+def cosine_beta_schedule(timesteps, s=0.008, raise_to_power: float = 1):
+    """
+    cosine schedule
+    as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
+    """
+    steps = timesteps + 2
+    x = np.linspace(0, steps, steps)
+    alphas_cumprod = np.cos(((x / steps) + s) / (1 + s) * np.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    betas = np.clip(betas, a_min=0, a_max=0.999)
+    alphas = 1. - betas
+    alphas_cumprod = np.cumprod(alphas, axis=0)
+
+    if raise_to_power != 1:
+        alphas_cumprod = np.power(alphas_cumprod, raise_to_power)
+
+    return alphas_cumprod
+
+
+def cosine_beta_schedule_discrete(timesteps, s=0.008):
+    """ Cosine schedule as proposed in https://openreview.net/forum?id=-NEXDKk8gZ. """
+    steps = timesteps + 2
+    x = np.linspace(0, steps, steps)
+
+    alphas_cumprod = np.cos(0.5 * np.pi * ((x / steps) + s) / (1 + s)) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    alphas = (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    betas = 1 - alphas
+    return betas.squeeze()
+
+
+def custom_beta_schedule_discrete(timesteps, average_num_nodes=50, s=0.008):
+    """ Cosine schedule as proposed in https://openreview.net/forum?id=-NEXDKk8gZ. """
+    steps = timesteps + 2
+    x = np.linspace(0, steps, steps)
+
+    alphas_cumprod = np.cos(0.5 * np.pi * ((x / steps) + s) / (1 + s)) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    alphas = (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    betas = 1 - alphas
+
+    assert timesteps >= 100
+
+    p = 4 / 5       # 1 - 1 / num_edge_classes
+    num_edges = average_num_nodes * (average_num_nodes - 1) / 2
+
+    # First 100 steps: only a few updates per graph
+    updates_per_graph = 1.2
+    beta_first = updates_per_graph / (p * num_edges)
+
+    betas[betas < beta_first] = beta_first
+    return np.array(betas)
+
+
+class PredefinedNoiseScheduleDiscrete(torch.nn.Module):
+    """
+    Predefined noise schedule. Essentially creates a lookup array for predefined (non-learned) noise schedules.
+    """
+
+    def __init__(self, noise_schedule, timesteps,noise_type,blosum_path='dataset_src/blosum_substitute.pt'):
+        super(PredefinedNoiseScheduleDiscrete, self).__init__()
+        self.timesteps = timesteps
+        self.noise_type = noise_type
+        if self.noise_type == 'blosum':
+            self.temperature_list = torch.load(blosum_path)['temperature']
+        if noise_schedule == 'cosine':
+            betas = cosine_beta_schedule_discrete(timesteps)
+        elif noise_schedule == 'custom':
+            betas = custom_beta_schedule_discrete(timesteps)
+        else:
+            raise NotImplementedError(noise_schedule)
+
+        self.register_buffer('betas', torch.from_numpy(betas).float())
+
+        self.alphas = 1 - torch.clamp(self.betas, min=0, max=0.9999)
+
+        log_alpha = torch.log(self.alphas)
+        log_alpha_bar = torch.cumsum(log_alpha, dim=0)
+        self.alphas_bar = torch.exp(log_alpha_bar)
+
+    def forward(self, t_normalized=None, t_int=None):
+        assert int(t_normalized is None) + int(t_int is None) == 1
+        if t_int is None:
+            t_int = torch.round(t_normalized * self.timesteps)
+        if self.betas.device != t_int.device:
+            self.betas = self.betas.to(t_int.device)
+        return self.betas[t_int.long()]
+
+    def get_alpha_bar(self, t_normalized=None, t_int=None):
+        if self.noise_type == 'uniform':
+            assert int(t_normalized is None) + int(t_int is None) == 1
+            if t_int is None:
+                t_int = torch.round(t_normalized * self.timesteps)
+            if self.alphas_bar.device != t_int.device:
+                self.alphas_bar = self.alphas_bar.to(t_int.device)
+            return self.alphas_bar[t_int.long()]
+        else:
+            return t_normalized
+
+
+
